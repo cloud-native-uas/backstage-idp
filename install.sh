d5cc70f021eeb6c5cadd101cfc6851b6d28454c7
Thomas Schuetz <thomas.schuetz@t-sc.eu>
2024-06-28 15:32:14 +0200

    feat: fix installation
    Signed-off-by: Thomas Schuetz <thomas.schuetz@t-sc.eu>

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 sudo apt update \
+&& sudo apt install -y npm \
 && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash \
 && . ~/.bashrc
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
-. ~/.bashrc nvm install 18 \
+nvm install 18 \
 && nvm use 18 \
+&& sudo npm install yarn bash-language-server -g \
 && cd mcce-idp \
 && yarn install --frozen-lockfile \
 && yarn tsc
